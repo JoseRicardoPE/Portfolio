54aa5a991686e253f170325471e6177001215742
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-02-23 12:38:29 -0500

    add styles contact component

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  phoneNumber: string = '+1234567890';
+  email = 'contact@example.com';
+  doNothing() {
+    console.log('Button clicked, but no action is defined.');
+  }
 }
