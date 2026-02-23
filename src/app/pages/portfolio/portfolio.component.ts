cb520f6bdef76726838e13be404cdbe5f89dd27f
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-02-23 12:37:39 -0500

    add styles portfolio component

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  doNothing() {
+    console.log('Nothing to see here!');
+  }
 }
